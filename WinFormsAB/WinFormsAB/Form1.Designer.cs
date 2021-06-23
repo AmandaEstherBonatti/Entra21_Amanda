ac45f5c8ea25750439fdb11eae892be019405cdc
Amanda Esther Bonatti <estudosbonatti@hotmail.com>
2021-06-23 19:44:55 -0300

    PrimeiroForms

---
+++ WinFormsAB/WinFormsAB/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace WinFormsAB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnMultiplicao = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSoma.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnSoma.Location = new System.Drawing.Point(230, 289);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(136, 53);
+            this.btnSoma.TabIndex = 0;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = false;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSubtracao.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnSubtracao.Location = new System.Drawing.Point(230, 220);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(136, 46);
+            this.btnSubtracao.TabIndex = 1;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = false;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_Click);
+            // 
+            // btnMultiplicao
+            // 
+            this.btnMultiplicao.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnMultiplicao.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnMultiplicao.Location = new System.Drawing.Point(429, 220);
+            this.btnMultiplicao.Name = "btnMultiplicao";
+            this.btnMultiplicao.Size = new System.Drawing.Size(149, 46);
+            this.btnMultiplicao.TabIndex = 2;
+            this.btnMultiplicao.Text = "*";
+            this.btnMultiplicao.UseVisualStyleBackColor = false;
+            this.btnMultiplicao.Click += new System.EventHandler(this.btnMultiplicao_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnDivisao.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnDivisao.Location = new System.Drawing.Point(429, 289);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(149, 53);
+            this.btnDivisao.TabIndex = 3;
+            this.btnDivisao.Text = "/";
+            this.btnDivisao.UseVisualStyleBackColor = false;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(230, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(429, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(317, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(471, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 465);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnMultiplicao);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnSoma);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnMultiplicao;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WinFormsAB/WinFormsAB/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAB
+{
+    public partial class Form1 : Form
+    {
+        int numero1, numero2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Seu resutado é {numero1 - numero2}");
+        }
+
+        private void btnMultiplicao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Seu resutado é {numero1 * numero2}");
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Seu resutado é {numero1 / numero2}");
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Seu resutado é {numero1 + numero2}");
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            numero2 = int.Parse(textBox2.Text);
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            numero1 = int.Parse(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
